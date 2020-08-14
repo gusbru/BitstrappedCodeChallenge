2de1b29745564635c537ef3998746aa9be80131c
Gustavo Brunetto <gustavobruneto@gmail.com>
2020-08-14 14:38:57 -0700

    implement the basics routes

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import Requests from "./API/Requests.js";
+
+const app = express();
+
+const RATES_API_URL = "https://api.ratesapi.io/api/latest";
+
+app.get("/", async (req, res) => {
+  try {
+    const ratesObj = await Requests.get(RATES_API_URL);
+    res.send(ratesObj.rates);
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).send(error.message);
+  }
+});
+
+app.get("/:country", async (req, res) => {
+  try {
+    const { country } = req.params;
+    const ratesObj = await Requests.get(RATES_API_URL);
+    console.log(country);
+    console.log(Object.keys(ratesObj.rates));
+    const countyKey = Object.keys(ratesObj.rates).find(
+      (item) => item.toLowerCase() === country.trim().toLowerCase()
+    );
+    console.log(countyKey);
+    const rate = ratesObj.rates[countyKey];
+    const ansObj = {
+      country: countyKey,
+      rate,
+    };
+
+    res.json(ansObj);
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).send(error.message);
+  }
+});
+
+app.listen(3030, () => {
+  console.log("server is running");
+});
